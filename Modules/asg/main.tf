0dba6efe8beff711ec9c2e35e293fb3484bc94df
YongFeng07 <yongfeng3318@gmail.com>
2026-09-12 20:34:55 +0800

    Update health check period

--- Modules/asg/main.tf
+++ Modules/asg/main.tf
@@ -63,7 +63,7 @@ resource "aws_autoscaling_group" "app" {
   max_size                  = var.max_size
   desired_capacity          = var.desired_capacity
   health_check_type         = "ELB"
-  health_check_grace_period = 300
+  health_check_grace_period = 600
   target_group_arns         = [var.target_group_arn]
 
   launch_template {
